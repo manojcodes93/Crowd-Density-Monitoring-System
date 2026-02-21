dec0b67755e627512ee4a811f4b23478e2765f81
Manoj Royal <manojroyal7805@gmail.com>
2026-02-21 16:48:52 +0530

    converted hardcoded camera to controllable one config file

--- test_detection.py
+++ test_detection.py
@@ -5,10 +5,30 @@ from app.density import calculate_zones
 from app.heatmap import generate_heatmap
 import csv
 from datetime import datetime
+import yaml
 
-cap = cv2.VideoCapture(0)
+# Load configuration
+with open("config/settings.yaml", "r") as file:
+    config = yaml.safe_load(file)
 
-frame_skip = 3
+camera_config = config["camera"]
+system_config = config["system"]
+prediction_config = config["prediction"]
+
+
+if camera_config["mode"] == "webcam":
+    cap = cv2.VideoCapture(camera_config["webcam_index"])
+
+elif camera_config["mode"] == "ip":
+    cap = cv2.VideoCapture(camera_config["ip_stream"])
+
+elif camera_config["mode"] == "video":
+    cap = cv2.VideoCapture(camera_config["video_path"])
+
+else:
+    raise ValueError("Invalid camera mode in settings.yaml")
+
+frame_skip = system_config["frame_skip"]
 frame_count = 0
 
 boxes = []
@@ -26,8 +46,8 @@ if not file_exists:
 last_logged_time = None
 
 recent_counts = []
-prediction_threshold = 10
-prediction_window = 5
+prediction_threshold = prediction_config["threshold"]
+prediction_window = prediction_config["window_seconds"]
 
 predicted_value = 0
 prediction_alert = False
@@ -38,7 +58,7 @@ while True:
         break
 
     # Resize for faster inference
-    frame = cv2.resize(frame, (640, 480))
+    frame = cv2.resize(frame, (system_config["frame_width"], system_config["frame_height"]))
 
     frame_count += 1
 
